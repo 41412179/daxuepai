c9d52e93b57aa4344995468df160930bb768321a
guohaodong <1593028064@qq.com>
2019-08-11 22:33:55 +0800

    init project

---
+++ gaoxiao/src/main/java/com/daxuepai/gaoxiao/GaoxiaoApplication.java
@@ -0,0 +1,13 @@
+package com.daxuepai.gaoxiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaoxiaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaoxiaoApplication.class, args);
+	}
+
+}
---
+++ gaoxiao/src/test/java/com/daxuepai/gaoxiao/GaoxiaoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.daxuepai.gaoxiao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GaoxiaoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

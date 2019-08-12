22e082187d8d3bebf81dfbba9d283af589d9d568
guohaodong <1593028064@qq.com>
2019-08-12 10:58:18 +0800

    add test helloword

---
+++ gaoxiao/src/main/java/com/daxuepai/gaoxiao/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.daxuepai.gaoxiao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test(ModelAndView modelAndView){
+        modelAndView.addObject("hello", "hello  world");
+        return "test";
+    }
+}
